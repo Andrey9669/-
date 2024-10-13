e77c38b0f1bb68bc354df1f7afeb2a42d694cd10
Andrey9669 <167240650+Andrey9669@users.noreply.github.com>
2024-10-13 13:04:35 +0200

    README.txt
    ну прочитай

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class  Main {
+    public static void main(String[] args) {
+        ArrayList<String> D2 = new ArrayList<>();
+            String a = "1.";
+            String b = "2.";
+            String c = "3.";
+
+            String[] players = {"Pudge", "Shadow Fiend", "Arc Warden"};
+
+            D2.add("\t\t\n\n\n\n\n\n\n\nDOTA2\n" + a + "place: " +  players[0] + "\t800.000.000 - DOLLARS" + "\n" + b + "place: " + players[1] + "\t400.000.000 - DOLLARS" + "\n" + c + "place: " + players[2] + "\t100.000.000 - DOLLARS" + "\n\n\n\n\n\n\n");
+
+        System.out.println(D2);
+
+    }
+}
